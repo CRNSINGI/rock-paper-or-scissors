854986a093ebf2d85fe981d2b42b8affacad3375
Cesar Nsingi <cesarricardo1805@hotmail.com>
2024-07-12 10:13:00 +0300

    math random and math floor

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ if(userInput === 'rock' || userInput === 'scissors' || userInput === 'paper') {
     console.log('Error, please type: rock,paper or scissors.');
 }
 } // user choice start of the gaome and  different capitalizations 
+
+const getComputerChoice = () => { 
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    switch (randomNumber) {
+        case 0: 
+          return 'rock';
+        case 1:
+          return 'paper';
+        case 2: 
+          return 'scissors';
+    }
+}; // math.random and math fllor to get the whole number from 0 to 2; 
