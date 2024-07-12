ad5973f29ec88854c81ee7312a8646a1650249e2
Cesar Nsingi <cesarricardo1805@hotmail.com>
2024-07-12 09:37:19 +0300

    if or else statement

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const getUserChoice = (userInput) => {
 userInput = userInput.toLowerCase(); 
+if(userInput === 'rock' || userInput === 'scissors' || userInput === 'paper') {
+    return userInput
+} else {
+    console.log('Error, please type: rock,paper or scissors.');
+}
 } // user choice start of the gaome and  different capitalizations 
